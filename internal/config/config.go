2dc7384ac8563abd726a59f081c662cf31863c3a
Bruno Hugo <brunohugo2007@gmail.com>
2026-03-14 08:54:34 -0300

    fix:

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,15 +1,12 @@
 package config
 
 type ConfigType struct {
-	DbUser     string
-	DbPassword string
-	DbPort     string
-	DbHost     string
-	DbDatabase string
-
-	JwtSecret string
-
-	Port string
-
-	FrontendHost string
+	DbUser       string `json:"db_user"`
+	DbPassword   string `json:"db_password"`
+	DbPort       string `json:"db_port"`
+	DbHost       string `json:"db_host"`
+	DbDatabase   string `json:"db_database"`
+	JwtSecret    string `json:"jwt_secret"`
+	Port         string `json:"port"`
+	FrontendHost string `json:"frontend_host"`
 }
